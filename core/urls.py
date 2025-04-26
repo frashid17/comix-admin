3f420885a2a96b01f2675489b5559b496ecb0ebd
Patrick Mwangi <frashid274@gmail.com>
2025-04-27 00:10:00 +0300

    Add URL configuration for backend and core applications

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import UserRegistrationView, ServiceListView, CreateOrderView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('services/', ServiceListView.as_view(), name='service_list'),
+    path('book/', CreateOrderView.as_view(), name='create_order'),
+]
