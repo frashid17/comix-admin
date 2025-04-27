ddb2ea0aa75846c8f4f2dbfdd1dec2d7ea6b38a8
Patrick Mwangi <frashid274@gmail.com>
2025-04-27 03:16:05 +0300

    Add user order and Expo push token endpoints to URL configuration

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegistrationView, ServiceListView, CreateOrderView
+from .views import UserRegistrationView, ServiceListView, CreateOrderView, UserOrderListView, SaveExpoPushTokenView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -8,4 +8,6 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('services/', ServiceListView.as_view(), name='service_list'),
     path('book/', CreateOrderView.as_view(), name='create_order'),
+    path('my-bookings/', UserOrderListView.as_view(), name='user_order_list'),
+    path('save-token/', SaveExpoPushTokenView.as_view(), name='save_expo_token'),
 ]
