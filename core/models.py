3720171721a9ec26a79c376cb971ebea68d7689b
Patrick Mwangi <frashid274@gmail.com>
2025-04-27 03:15:48 +0300

    Add user profile creation on user registration

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 class Service(models.Model):
     name = models.CharField(max_length=255)
@@ -40,6 +42,13 @@ class Profile(models.Model):
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    expo_push_token = models.CharField(max_length=255, blank=True, null=True)
+
 
     def __str__(self):
         return self.user.username
+    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
