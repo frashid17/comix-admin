a6fd5348d0f86e549bfae141ac4b4b7c2e9831d8
Patrick Mwangi <frashid274@gmail.com>
2025-04-28 04:07:47 +0300

    Add static media URL handling to URL configuration

--- backend/urls.py
+++ backend/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from rest_framework import permissions
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
