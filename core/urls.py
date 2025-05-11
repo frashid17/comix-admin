d4db1921de04261276f02147b0a4839f603fbde9
Patrick Mwangi <frashid274@gmail.com>
2025-05-11 05:06:01 +0300

    Add feedback and profile endpoints to the API

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,10 @@ ServiceListView,
 CreateOrderView, 
 UserOrderListView, 
 SaveExpoPushTokenView,
-ProductListView
+ProductListView,
+UserProfileView,
+FeedbackCreateView
+
 )
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -17,5 +20,7 @@ urlpatterns = [
     path('book/', CreateOrderView.as_view(), name='create_order'),
     path('my-bookings/', UserOrderListView.as_view(), name='user_order_list'),
     path('save-token/', SaveExpoPushTokenView.as_view(), name='save_expo_token'),
-     path('products/', ProductListView.as_view(), name='products'),
+    path('products/', ProductListView.as_view(), name='products'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+    path('feedback/', FeedbackCreateView.as_view(), name='submit-feedback'),
 ]
